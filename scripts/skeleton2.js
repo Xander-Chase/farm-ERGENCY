592e072343e49b81d697e3aefe24f89e121e86b5
Markus Lum-De Guzman <markusldg@hotmail.com>
2023-03-02 02:13:24 -0800

    Markus - Added javascript functions for the authentication API, firebase API, and footer skeletons. Added an index.html for a login page, a login.html for logging in/signing up, and a signup.html for potential ui changes later. Added footer.html's to load the skeleton, and added javascript functions to each html page.

---
+++ scripts/skeleton2.js
@@ -0,0 +1,8 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton2(){
+    console.log($('#footerlogin').load('./text/footerlogin.html'));
+}
+loadSkeleton2();  //invoke the function
