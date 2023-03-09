cb6322ff978e634530dcdb55a9175e68b596eb96
Markus Lum-De Guzman <markusldg@hotmail.com>
2023-03-08 23:42:23 -0800

    Added javascript so that user collection is added to firebase database after sign in. Added javascript and html so that editing and submitting personal details writes into our firebase database. Framework of reading into the database and displaying it into personal details after submitting is made.

--- scripts/personal_Details.js
+++ scripts/personal_Details.js
@@ -1,3 +1,5 @@
+var currentUser;          //put this right after you start script tag before writing any functions.
+
 function getNameFromAuth() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
@@ -31,20 +33,87 @@ auth.signInWithEmailAndPassword(email, password)
 
   const contactForm = document.getElementById('contact-form');
 
-contactForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  
-  const formData = new FormData(contactForm);
-  
-  const fullName = formData.get('full-name');
-  const email = formData.get('email');
-  const phone = formData.get('phone');
-  const mobile = formData.get('mobile');
-  const address = formData.get('address');
-  
-  console.log('Full Name:', fullName);
-  console.log('Email:', email);
-  console.log('Phone:', phone);
-  console.log('Mobile:', mobile);
-  console.log('Address:', address);
-});
+  function populateUserInfo() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("userinfo").doc(user.uid)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    //get the data fields of the user
+                    var user_name = userDoc.data().Name;
+                    var user_email = userDoc.data().userEmail;
+                    var user_phone = userDoc.data().userPhone;
+                    var user_mobile = userDoc.data().userMobile;
+                    var user_address = userDoc.data().userAddress;
+
+                    //if the data fields are not empty, then write them in to the form.
+                    if (fullName != null) {
+                        document.getElementById("full-name").value = user_name;
+                    }
+                    if (userEmail != null) {
+                        document.getElementById("email").value = user_email;
+                    }
+                    if (userPhone != null) {
+                        document.getElementById("phone").value = user_phone;
+                    }
+                    if (userMobile != null) {
+                        document.getElementById("mobile").value = user_mobile;
+                    }
+                    if (userAddress != null) {
+                        document.getElementById("address").value = user_address;
+                    }
+                })
+        } else {
+            // No user is signed in.
+            console.log ("No user is signed in");
+        }
+    });
+}
+
+//call the function to run it 
+window.onload = populateUserInfo;
+
+function editUserInfo() {
+  //Enable the form fields
+  document.getElementById('personalInfoFields').disabled = false;
+}
+
+function saveUserInfo() {
+  let fullName = document.getElementById("full-name").value;
+  let userEmail = document.getElementById("email").value;
+  let userMobile = document.getElementById("mobile").value;
+  let userPhone = document.getElementById("phone").value;
+  let userAddress = document.getElementById("address").value;
+  console.log(fullName, userEmail, userMobile, userPhone, userAddress);
+
+  firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+          var currentUser = db.collection("users").doc(user.uid)
+          var userID = user.uid;
+          //get the document for current user.
+          currentUser.get()
+              .then(userDoc => {
+                  var userEmail = userDoc.data().email;
+                  db.collection("userinfo").add({
+                      userID: userID,
+                      Name: fullName,
+                      email: userEmail,
+                      mobile: userMobile,
+                      phone: userPhone,
+                      address: userAddress,
+                      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                  }).then(() => {
+                      window.location.href = "personalDetails.html"; //new line added
+                  })
+              })
+      } else {
+          console.log("No user is signed in");
+          window.location.href = 'personalDetails.html';
+      }
+  });
+}
+
