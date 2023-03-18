8b5fabf36527e023d3597dc46d028b0a4a3738a1
Alex Gabl <agablbeausejour@my.bcit.ca>
2023-03-17 18:39:49 -0700

    notifications.js added. Notifications now show user submitted FARMergency forms in descending order based on timestamp. UserID will need to link to a profile screen that displays that users info.

---
+++ scripts/notifications.js
@@ -0,0 +1,24 @@
+db.collection("alertForm")
+  .orderBy("timestamp", "desc")
+  .get()
+  .then((querySnapshot) => {
+    const tableBody = document.getElementById("submissions").getElementsByTagName("tbody")[0];
+    // Loop through each submission and add a row to the table
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      const timestamp = data.timestamp.toDate().toLocaleString();
+      const row = `
+        <tr>
+          <td>${data.userID}</td>
+          <td>${data.evacuation}</td>
+          <td>${data.transport}</td>
+          <td>${data.property}</td>
+          <td>${timestamp}</td>
+        </tr>
+      `;
+      tableBody.innerHTML += row;
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching submissions:", error);
+  });
