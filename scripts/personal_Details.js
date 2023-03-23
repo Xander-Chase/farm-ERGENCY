0c145ac7910729f6e084d56a3df523ec729085bd
Alex Gabl <agablbeausejour@my.bcit.ca>
2023-03-22 23:32:57 -0700

    added geocoding functionality to personal_Details.js. User entered address now gets geocoded to lat and long coordinates and saved to firestore in the userinfo collection

--- scripts/personal_Details.js
+++ scripts/personal_Details.js
@@ -1,58 +1,84 @@
-var currentUser;          //put this right after you start script tag before writing any functions.
+var currentUser;
 
 function getNameFromAuth() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
-        if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
-            const userName = user.displayName;
-
-            //method #1:  insert with JS
-            document.getElementById("name-goes-here").innerText = userName;   
-
-        } else {
-            // No user is signed in.
-        }
-    });
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log(user.uid);
+      console.log(user.displayName);
+      const userName = user.displayName;
+      document.getElementById("name-goes-here").innerText = userName;
+    } else {
+      // No user is signed in.
+    }
+  });
 }
-getNameFromAuth(); //run the function
 
+getNameFromAuth();
 
 function populateUserInfo() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        var userID = user.uid;
-        db.collection("userinfo")
-          .where("userID", "==", userID)
-          .orderBy("timestamp", "desc")
-          .limit(1)
-          .get()
-          .then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-              const data = doc.data();
-              document.getElementById("full-name").value = data.Name;
-              document.getElementById("email").value = data.email;
-              document.getElementById("mobile").value = data.mobile;
-              document.getElementById("phone").value = data.phone;
-              document.getElementById("address").value = data.address;
-            });
-          })
-          .catch(error => {
-            console.log("Error getting user info: ", error);
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      var userID = user.uid;
+      db.collection("userinfo")
+        .where("userID", "==", userID)
+        .orderBy("timestamp", "desc")
+        .limit(1)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            const data = doc.data();
+            document.getElementById("full-name").value = data.Name;
+            document.getElementById("email").value = data.email;
+            document.getElementById("mobile").value = data.mobile;
+            document.getElementById("phone").value = data.phone;
+            document.getElementById("address").value = data.address;
           });
-      } else {
-        console.log("No user is signed in");
-      }
-    });
-  }
-  
-  //call the function to run it 
+        })
+        .catch(error => {
+          console.log("Error getting user info: ", error);
+        });
+    } else {
+      console.log("No user is signed in");
+    }
+  });
+}
+
 populateUserInfo();
 
+function saveUserLongLat(userID, longitude, latitude) {
+  db.collection("users").doc(userID).update({
+    longitude: longitude,
+    latitude: latitude
+  })
+  .then(() => {
+    console.log("Longitude and latitude saved to Firestore");
+  })
+  .catch((error) => {
+    console.error("Error saving longitude and latitude: ", error);
+  });
+}
+
+function geocodeAddress(address, userID) {
+  
+  const geocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiYWpnYWJsIiwiYSI6ImNsZXFyN2pmaDBsbmQzcmxrdDN1bWR2dWQifQ.X2m-VshHfJA_ZpBixUPCaw`;
+  // console.log("geocoding lat and long", geocodeAddress);
+    fetch(geocodingUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Get the longitude and latitude from the geocoder
+      const longitude = data.features[0].center[0];
+      const latitude = data.features[0].center[1];
+
+      // Save the longitude and latitude to Firestore
+      saveUserLongLat(userID, longitude, latitude);
+    })
+    .catch(error => {
+      console.log("Error geocoding address: ", error);
+    });
+}
+
+
 function editUserInfo() {
-  //Enable the form fields
   document.getElementById('personalInfoFields').disabled = false;
 }
 
@@ -62,31 +88,54 @@ function saveUserInfo() {
   let userMobile = document.getElementById("mobile").value;
   let userPhone = document.getElementById("phone").value;
   let userAddress = document.getElementById("address").value;
+
   console.log(fullName, userEmail, userMobile, userPhone, userAddress);
 
   firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-          var currentUser = db.collection("users").doc(user.uid)
-          var userID = user.uid;
-          //get the document for current user.
-          currentUser.get()
-              .then(userDoc => {
-                  db.collection("userinfo").add({
-                      userID: userID,
-                      Name: fullName,
-                      email: userEmail,
-                      mobile: userMobile,
-                      phone: userPhone,
-                      address: userAddress,
-                      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-                  }).then(() => {
-                      window.location.href = "personalDetails.html"; //new line added
-                  })
+    if (user) {
+      var currentUser = db.collection("users").doc(user.uid);
+      var userID = user.uid;
+      //get the document for current user.
+      currentUser.get()
+        .then(userDoc => {
+          db.collection("userinfo").add({
+            userID: userID,
+            Name: fullName,
+            email: userEmail,
+            mobile: userMobile,
+            phone: userPhone,
+            address: userAddress,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+          }).then(docRef => {
+            const geocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${userAddress}.json?access_token=pk.eyJ1IjoiYWpnYWJsIiwiYSI6ImNsZXFyN2pmaDBsbmQzcmxrdDN1bWR2dWQifQ.X2m-VshHfJA_ZpBixUPCaw`;
+            fetch(geocodingUrl)
+              .then(response => response.json())
+              .then(data => {
+                // Get the longitude and latitude from the geocoder
+                const longitude = data.features[0].center[0];
+                const latitude = data.features[0].center[1];
+
+                // Save the longitude and latitude to Firestore
+                docRef.update({
+                  longitude: longitude,
+                  latitude: latitude
+                }).then(() => {
+                  console.log("Longitude and latitude saved to Firestore");
+                  window.location.href = "personalDetails.html";
+                }).catch(error => {
+                  console.log("Error updating document: ", error);
+                });
               })
-      } else {
-          console.log("No user is signed in");
-          window.location.href = 'personalDetails.html';
-      }
+              .catch(error => {
+                console.log("Error geocoding address: ", error);
+              });
+          }).catch(error => {
+            console.log("Error adding document: ", error);
+          });
+        });
+    } else {
+      console.log("No user is signed in");
+      window.location.href = 'personalDetails.html';
+    }
   });
 }
-
